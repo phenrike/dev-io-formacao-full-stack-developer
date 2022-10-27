4adff4a499479e4e911258b9f0cf1662bd9b6726
Paulo Piaunios <paulosantossjc@hotmail.com>
2022-10-26 22:27:42 -0300

    Dominando o ASP.NET MVC Core - Views
    Utilizando Scaffold

---
+++ EntendendoMvc/EntendendoMvc/Data/EntendendoMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntendendoMvc.Models;
+
+namespace EntendendoMvc.Data
+{
+    public class EntendendoMvcContext : DbContext
+    {
+        public EntendendoMvcContext (DbContextOptions<EntendendoMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntendendoMvc.Models.Filme> Filme { get; set; } = default!;
+    }
+}
--- EntendendoMvc/EntendendoMvc/Program.cs
+++ EntendendoMvc/EntendendoMvc/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using EntendendoMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<EntendendoMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EntendendoMvcContext") ?? throw new InvalidOperationException("Connection string 'EntendendoMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
